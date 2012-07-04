c94636c6463e883ee67963b0906a7a0572cfa94b
Wenebunha Etitinwo <weniswow15@yahoo.com>
2012-07-04 21:19:55 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloakedArcher::Application.config.secret_token = 'a3c467fbe446134f59d7c04b63dacff6eda01368490eb787150505caddc8729d75b5d28e0a7229e0af58d51eb46e20fdf4d38b5523d92b7cd6c4af27d577c92a'
